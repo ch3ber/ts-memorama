a8c1a3fba2032efabbfd2c5a606e4293d413c84d
Eber Alejo <eber4202@hotmail.com>
2023-06-27 22:55:57 -0600

    ADD: createCardId util

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,5 +1,5 @@
 import Card from "@/components/Card";
-import { createCardId } from "@/utils/crateCardId";
+import { createCardId } from "@/utils/createCardId";
 
 type CardGridProps = {
   order: number[]
@@ -9,8 +9,8 @@ export default function CardGrid({ order }: CardGridProps) {
   return (
     <section className="max-w-6xl mx-auto items-center justify-center grid grid-cols-2 lg:grid-cols-4 gap-14 mt-10">
       {order.map((numberCard) => {
-        const id = createCardId()
-        return <Card key={id} number={numberCard} id={id} />
+        const cardId = createCardId()
+        return <Card key={cardId} number={numberCard} id={cardId} />
       })}
     </section>
   )
